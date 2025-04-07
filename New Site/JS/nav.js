9d0e88562717c16756cc18fbc05ecf936bbb83c9
arputhamips <arputhamips@users.noreply.github.com>
2025-04-06 23:55:47 -0400

    new page Nav Feature Compplete

---
+++ New Site/JS/nav.js
@@ -0,0 +1,35 @@
+
+const navLinks = document.getElementById("navlinks");
+const btnIcon = document.getElementById("btn-icon");
+
+isOpen = false;
+
+function openNav() {
+    navLinks.classList.add("nav-open");
+    btnIcon.classList.replace("fa-bars","fa-xmark");
+    isOpen = true;
+  }
+
+function closeNav() {
+    navLinks.classList.remove("nav-open");
+    navLinks.classList.add("nav-close");
+    btnIcon.classList.replace("fa-xmark","fa-bars"); 
+    isOpen = false;
+    setTimeout(() => {
+      navLinks.classList.remove("nav-close");
+    }, 2000);
+  }
+
+function ToggleNav() {
+  isMobileSize = window.matchMedia("(max-width: 560px)").matches;
+  console.log(isMobileSize);
+  
+  if (isMobileSize) {
+    if (!isOpen) {
+      openNav();   
+    } else {
+      closeNav();
+    }
+  }
+
+}
